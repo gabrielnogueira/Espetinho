c353db17790c3a0c48a40311c970c22cc7f8eebb
Gabriel Nogueira <infotisolutions@gmail.com>
2015-11-24 14:29:09 -0200

    Comit Menu Testado Android

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,32 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+ 
 
-.run(function($ionicPlatform) {
+angular.module('starter', ['ionic']).controller('MyCtrl', function($scope, $document) {
+  
+  $scope.items = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 }
+  ];
+  
+  $scope.toggleTopMenu = function () {
+    var menu = document.getElementById('menuTop');
+    var menuBar = document.getElementById('menuBar');
+    menu.style.height = menuBar.style.top = (menu.offsetHeight==0)?'100%':'0px';
+  };
+
+  $scope.clickItemMenu = function () {
+    var menu = document.getElementById('menuTop');
+    var menuBar = document.getElementById('menuBar');
+    menu.style.height = menuBar.style.top = '0px';
+
+  };
+  
+})
+  .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +40,4 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
