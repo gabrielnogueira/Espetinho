2f964cdf990a4db1aaa29b004c5cc06c878c4fd7
Gabriel Nogueira <infotisolutions@gmail.com>
2015-11-26 19:26:44 -0200

    Menu Auxiliar 80% Pronto, faltando apenas detalhes

--- www/js/app.js
+++ www/js/app.js
@@ -9,10 +9,21 @@ angular.module('starter', ['ionic']).controller('MyCtrl', function($scope, $docu
   
 
 
+$scope.testeTreme = function () {
+    var div = document.getElementById('tremer');
+    div.classList.add('shake-constant');
+}
+
   $scope.toggleTopMenu = function () {
    
     var menu = document.getElementById('menuTop');
     var menuBar = document.getElementById('menuBar');
+
+    /*menu.classList.remove('transitionsMenuOut');
+    menuBar.classList.remove('transitionsMenuOut');
+    menu.classList.add('transitionsMenu');
+    menuBar.classList.add('transitionsMenu');*/
+
     var topMenu = menu.style.top;
     var topMenuBar = menuBar.style.top;
 
@@ -31,18 +42,73 @@ angular.module('starter', ['ionic']).controller('MyCtrl', function($scope, $docu
 
     var totalHeight = menu.offsetHeight;
 
-    toggleMenuItens(totalHeight);
+    var transitionEnd = transitionEndEventName();
+
+    function funcionTremer(){
+        fazTremer('teste6'); 
+
+        toggleMenuItensTest(totalHeight, 1, document.getElementById('scrollPanel').children[0].children.length-1);
+
+        document.getElementById('menuBar').removeEventListener(transitionEnd, funcionTremer, false);
+    }
+
+    document.getElementById('menuBar').addEventListener(transitionEnd, funcionTremer, false);
+
+    toggleMenuItensTest(totalHeight, 0, document.getElementById('scrollPanel').children[0].children.length);
+
+
+/*    toggleMenuItensTest(totalHeight, 0, document.getElementById('scrollPanel').children[0].children.length);
+*/
+    //toggleMenuItens(totalHeight);
+    
 
   };
 
   $scope.clickItemMenu = function () {
     var menu = document.getElementById('menuTop');
     var menuBar = document.getElementById('menuBar');
+
+    /*menu.classList.remove('transitionsMenu');
+    menuBar.classList.remove('transitionsMenu');
+    menu.classList.add('transitionsMenuOut');
+    menuBar.classList.add('transitionsMenuOut');*/
+
+    var transitionEnd = transitionEndEventName();
+    document.getElementById('menuBar').addEventListener(transitionEnd, resetaMenu, false);
+
      menu.style.top = '-100%';
      menuBar.style.top = '0px';
 
 
+     var children = document.getElementById('scrollPanel').children[0].children;
+
+     function resetaMenu(){
+        for (var i = 0; i < children.length; i++) {
+           var tableChild = document.getElementById(children[i].id);
+           tableChild.classList.remove('transitionsMenuItem');    
+           tableChild.children[0].children[0].classList.remove('shake-stop');
+           tableChild.children[0].children[0].classList.remove('shake-constant');
+           tableChild.style.top = '-2000px';
+        };
+
+        document.getElementById('menuBar').removeEventListener(transitionEnd, resetaMenu, false);
+     }
+
+     /*var processor = setInterval(function(){
+ 
      // ITERAR NOS MENUS E VOLTA-LOS PRO TOP E HEIGHT ORIGINAIS
+     
+        for (var i = 0; i < children.length; i++) {
+           var tableChild = document.getElementById(children[i].id);
+           tableChild.classList.remove('transitionsMenuItem');    
+           tableChild.children[0].children[0].classList.remove('shake-stop');
+           tableChild.children[0].children[0].classList.remove('shake-constant');
+           tableChild.style.top = '-2000px';
+        };
+
+        clearInterval(processor); 
+        
+    }, 100);*/
      
      
   };
@@ -60,6 +126,79 @@ angular.module('starter', ['ionic']).controller('MyCtrl', function($scope, $docu
 })
 
 
+function fazTremer(id){
+ var div = document.getElementById(id).children[0].children[0];
+ div.classList.add('shake-stop');
+ div.classList.add('shake-constant');
+
+}
+
+function chamaProximo(totalHeight, valueJ, valueI){
+
+    if(valueI > 0){
+        toggleMenuItensTest(totalHeight, valueJ, valueI);
+    }
+}
+
+
+function toggleMenuItensTest(totalHeight, valueJ, valueI) {
+
+
+    console.log(valueJ, valueI);
+
+    var children = document.getElementById('scrollPanel').children[0].children;
+
+    var menu = document.getElementById('menuTop');
+    
+    var heightChild = totalHeight/children.length;
+    var widthChild = menu.offsetWidth + 200;
+    var j = valueJ;
+    var i = valueI;
+
+    j++;
+
+    var tableChild = children[i-1];
+    var qtdFilhos = children.length;
+    var topAtual = heightChild*(j-1)*-1;   
+
+    tableChild.style.height = totalHeight + 'px';
+
+
+    
+
+    var index = j*1000
+
+    tableChild.children[0].children[0].style.width = widthChild+ 'px';
+    tableChild.children[0].children[0].style.left = '-100px';
+    
+    if(i==children.length){
+         tableChild.classList.remove('transitionsMenuItem');
+         tableChild.style.top = '0px';
+         tableChild.children[0].children[0].style.height = heightChild+ 'px';
+         
+
+         //chamaProximo(totalHeight, j, i-1);
+    }else{
+
+        tableChild.classList.add('transitionsMenuItem');
+            
+        tableChild.style.top =  topAtual+'px'; 
+
+        tableChild.style.zIndex = index;
+
+        tableChild.children[0].children[0].style.height = heightChild + 'px';
+
+        var transitionEnd = transitionEndEventName();
+        tableChild.addEventListener(transitionEnd, chamarEventos, false);
+
+    }
+
+    function chamarEventos(){
+        fazTremer(tableChild.id);
+        chamaProximo(totalHeight, j, i-1);
+    }
+
+}
 
 function toggleMenuItens(totalHeight) {
     var children = document.getElementById('scrollPanel').children[0].children;
@@ -67,66 +206,135 @@ function toggleMenuItens(totalHeight) {
     var j = 0;
     var array = [""];
 
-    console.log('Qtd Filhos: ' + children.length);
 
     for (var i = children.length; i > 0; i--) {
         j++;
         var tableChild = children[i-1];
         var qtdFilhos = children.length;
-        var topAtual = heightChild*(i-1);   
-       
+        var topAtual = heightChild*(j-1)*-1;   
+
+        //console.log(topAtual)
+        //
+        
+        //
         tableChild.style.height = totalHeight + 'px';
 
-        console.log(totalHeight)
-
-        var h = totalHeight-((j-1)*heightChild);
-
         var index = j*1000
+        
+        if(i==children.length){
+             tableChild.classList.remove('transitionsMenuItem');
+             tableChild.style.top = '0px';
+             tableChild.children[0].children[0].style.height = heightChild+ 'px';
+        }else{
 
-        var item = {id:tableChild.id, topValue:topAtual, heightC:h, idx:index};
+            tableChild.classList.add('transitionsMenuItem');
+
+            var transitionEnd = transitionEndEventName();
+            tableChild.addEventListener(transitionEnd, function(){fazTremer(tableChild.id);}, false);
+                
+            tableChild.style.top =  topAtual+'px'; 
+
+            tableChild.style.zIndex = index;
+
+            tableChild.children[0].children[0].style.height = heightChild + 'px';
 
 
-        array[j-1] = item;
+        }
+
+
+      /*  var h = totalHeight-((j-1)*heightChild);
+
+        
+
+        var item = {id:tableChild.id, topValue:topAtual, heightC:heightChild, idx:index};
+
+
+        array[j-1] = item;*/
         
     }
 
-    sleepToogleMenuItem(array,200);
+
+   // var i = 1;
+
+    
+
+
+
+   //sleepToogleMenuItem(array,250,2);
 }
 
-function sleepToogleMenuItem(arrayItens, intervalTime) {
+function sleepToogleMenuItem(arrayItens, intervalTime, init){
 
-   var i=0;
+   var i=init;
    var limit = arrayItens.length;
-
-   console.log(limit);
 
    var processor = setInterval(function(){
 
+        console.log(i)
+        console.log(limit)
+
         var tableChild = document.getElementById(arrayItens[i].id);
+
+
+        tableChild.classList.add('transitionsMenuItem');
+
+        console.log('add o faz tremer no array :' + i + '/ id : ' + tableChild.id)
+        var transitionEnd = transitionEndEventName();
+        tableChild.addEventListener(transitionEnd, function(){fazTremer(tableChild.id); console.log('recebi '+tableChild.id);}, false);
             
         var topAtual = arrayItens[i].topValue;
 
         var h = arrayItens[i].heightC;
 
-        tableChild.style.top =  '0px'//topAtual+'px'; 
+        tableChild.style.top =  topAtual+'px'; 
 
         tableChild.style.zIndex = arrayItens[i].idx;
 
-        tableChild.style.height = h +'px';
+        tableChild.children[0].children[0].style.height = arrayItens[i].heightC + 'px';
 
        if(++i >= limit){ 
             clearInterval(processor); 
         } 
+
+        if(intervalTime == .1){
+            clearInterval(processor); 
+            sleepToogleMenuItem(arrayItens,200,i++);
+        }
         
     }, intervalTime);
 }
 
+function alertTransitionFinish(){
+    alert( "Finished transition!" );
+}
 
-//Fazer a função que gera o bounce, que é a que mexe no TOP e no Height do Anterior
 
-//FAZER O PRIMEIRO ITEM DESCER JUNTO COM A MENUBAR
+function transitionEndEventName () {
+    var i,
+        undefined,
+        el = document.createElement('div'),
+        transitions = {
+            'transition':'transitionend',
+            'OTransition':'otransitionend',  // oTransitionEnd in very old Opera
+            'MozTransition':'transitionend',
+            'WebkitTransition':'webkitTransitionEnd'
+        };
 
-//ESCOLHER A MELHOR FUNCAO E QUAL USAR
+    for (i in transitions) {
+        if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+            return transitions[i];
+        }
+    }
+
+    //TODO: throw 'TransitionEnd event is not supported in this browser'; 
+}
+
+
+//CRIAR SHAKE IGUAL DO EXEMPLO
+//FAZER SHAKE DIFERENCIADO PARA PRIMEIRO ITEM QUE VAI JUNTO COM MENU IGUAL NO EXEMPLo
+//FAZER DIVS BONITAS PARA MELHOR VISUALIZAÇÃO DO LAYOUT
+//ACERTAR TEMPO DOS EFEITOS
+
 
 
 
